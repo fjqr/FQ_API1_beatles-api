f64999ffd15f6eb8e7bd7e19cec0c72c4446bc93
Francisco Quintero <franperc@gmail.com>
2022-05-03 20:22:36 -0400

    getSongs, songs routes

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -1,22 +1,22 @@
 const pool = require('../db')
 
-const getBeatles = async (req,res,next) => {
+const getBeatles = async (req, res, next) => {
     try {
-        
+
         const beatles = await pool.query('SELECT * FROM MUSICIANS')
         res.status(200).json(beatles.rows)
-        
+
     } catch (error) {
         next(error)
     }
 }
 
-const getAlbums = async (req,res,next) => {
+const getAlbums = async (req, res, next) => {
     try {
-      
+
         const albums = await pool.query('SELECT * FROM ALBUMS')
         res.status(200).json(albums.rows)
-        
+
     } catch (error) {
         next(error)
     }
@@ -24,20 +24,49 @@ const getAlbums = async (req,res,next) => {
 
 const getAnAlbum = async (req, res, next) => {
     try {
-        const {id} = req.params
-    const album = await pool.query('SELECT * FROM Albums WHERE id = $1',[id])
-    res.status(200).json(album.rows)
+        const { id } = req.params
+        const album = await pool.query('SELECT * FROM Albums WHERE id = $1', [id])
+        res.status(200).json(album.rows)
     } catch (error) {
         next(error)
     }
-    
+
 }
 
-const createAlbum = async (req,res,next) => {
+const createAlbum = async (req, res, next) => {
     try {
-        const {year,title,type} = req.value
-        const newAlbum = await pool.query('INSERT INTO albums (year,title,type) VALUES($1,$2,$3) RETURNING',[year,title,type])
-        res.status(200).json(newAlbum.rows)
+        const { year, title, type } = req.body
+        const newAlbum = await pool.query('INSERT INTO albums (year,title,type) VALUES($1,$2,$3) RETURNING *', [year, title, type])
+        res.json(newAlbum.rows)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const createSong = async (req, res, next) => {
+    try {
+        const { title, duration, albumid, lyrics, musiciansid, leadsinger } = req.body
+        const newSong = await pool.query('INSERT INTO songs(title,duration,albumid,lyrics,musiciansid,leadsinger) VALUES($1,$2,$3,$4,$5,$6) RETURNING *', [title, duration, albumid, lyrics, musiciansid,leadsinger])
+        res.json(newSong.rows)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const getASong = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const song = await pool.query('SELECT * FROM songs WHERE id = $1', [id])
+        res.json(song.rows)
+    } catch (error) {
+        next(error)
+    }
+}
+
+const albumSongs = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const albumSongs = await pool.query('SELECT * FROM songs INNER JOIN albums ON songs.albumid = albums.id')
     } catch (error) {
         next(error)
     }
@@ -47,5 +76,9 @@ const createAlbum = async (req,res,next) => {
 module.exports = {
     getAlbums,
     getBeatles,
-    getAnAlbum
+    getAnAlbum,
+    createAlbum,
+    createSong,
+    getASong,
+    albumSongs
 }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,5 +1,5 @@
-const {Router} = require('express')
-const {getAlbums, getBeatles,getAnAlbum} = require('../controllers/tasks.controllers')
+const { Router } = require('express')
+const { getAlbums, getBeatles, getAnAlbum, createAlbum, createSong, getASong, albumSongs } = require('../controllers/tasks.controllers')
 
 const router = Router()
 
@@ -8,19 +8,18 @@ router.get('/albums', getAlbums)
 
 router.get('/beatles', getBeatles)
 router.get('/getalbum/:id', getAnAlbum)
+router.post('/createalbum', createAlbum)
+router.post('/createsong', createSong)
+router.get('/getsong/:id', getASong)
+router.get('/albumsongs/:id', albumSongs)
 
 
-router.get('/', (res,req) => {
+router.put('/', (res, req) => {
     res.send('Hola')
 })
 
 
-router.put('/', (res,req) => {
-    res.send('Hola')
-})
-
-
-router.delete('/', (res,req) => {
+router.delete('/', (res, req) => {
     res.send('Hola')
 })
 
