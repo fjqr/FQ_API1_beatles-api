51e247c5c61b4ea49169a8e84d96e7edea414f11
Francisco Quintero <franperc@gmail.com>
2022-06-03 16:56:57 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const taskRoutes = require('./routes/task.routes')
 
-const app =  express()
-const port = 3000
+const app = express()
+//const port = 3000
+const port = 5432
 
 app.use(cors())
 app.use(express.json())
@@ -17,17 +18,17 @@ app.use(
     })
 )
 
-app.get('/',(req,res) =>{
-    res.json({data: res})
+app.get('/', (req, res) => {
+    res.json({ data: res })
 })
 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
     return res.json({
         message: err.message
     })
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`App corriendo en el puerto ${port}`)
 })
 
